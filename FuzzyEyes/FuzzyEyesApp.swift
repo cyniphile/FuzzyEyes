5eec5cbb83db67455216d90b02ae7fe9e77d69c3
Luke Schiefelbein <luke.schiefelbein@gmail.com>
2024-11-10 18:55:59 -0500

    Initial Commit

---
+++ FuzzyEyes/FuzzyEyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuzzyEyesApp.swift
+//  FuzzyEyes
+//
+//  Created by Luke Schiefelbein on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FuzzyEyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FuzzyEyesTests/FuzzyEyesTests.swift
@@ -0,0 +1,16 @@
+//
+//  FuzzyEyesTests.swift
+//  FuzzyEyesTests
+//
+//  Created by Luke Schiefelbein on 11/10/24.
+//
+
+import Testing
+
+struct FuzzyEyesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
